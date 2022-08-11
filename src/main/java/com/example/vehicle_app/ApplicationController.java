ac57e78ed48c2f580d08ec77216756453bf28997
Himanshu Mendapara <himanshumendapra@gmail.com>
2022-08-11 22:14:32 +0530

    Update ApplicationController.java

--- src/main/java/com/example/vehicle_app/ApplicationController.java
+++ src/main/java/com/example/vehicle_app/ApplicationController.java
@@ -1,9 +1,9 @@
 package com.example.vehicle_app;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@RestController
 public class ApplicationController {
 
     @GetMapping("/index")
